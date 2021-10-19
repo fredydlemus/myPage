36a5c77b364ba0ba015c80c0ef54f203c573ec3a
fredyflemus <fredyflemus@gmail.com>
2021-10-19 00:05:27 -0600

    [UPDATE] welcome message

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@ const App = () => {
         <div>
             <h1>React Site under construction</h1>
             <h2>Comback Soon!!! :D</h2>
+            <h3>19/10/2021</h3>
         </div>
     );
 }
